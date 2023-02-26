932acab194fac718fd1c43eb9710df77951f221c
Davr1 <42148912+Davr1@users.noreply.github.com>
2023-02-26 13:38:37 +0100

    add autocomplete options

--- MoreSearchOperators/index.ts
+++ MoreSearchOperators/index.ts
@@ -25,9 +25,10 @@ interface SearchFilter {
     validator: (match: any) => boolean;
     getAutocompletions: (match: any) => { text: string; }[];
     _title: string;
+    _options: string;
 }
 
-interface AnswerFilter {
+interface SearchAnswer {
     componentType: "ANSWER";
     regex: RegExp;
     validator: (match: any) => boolean;
@@ -37,19 +38,55 @@ interface AnswerFilter {
     _dataKey: string | number;
 }
 
-interface CustomFilters {
-    [name: string]: SearchFilter | AnswerFilter;
-}
-
-const searchOperators: CustomFilters = {
+const searchOperators: {[name: string]: SearchFilter} = {
     FILTER_SORT_ORDER: {
         componentType: "FILTER",
         regex: /sortOrder:/i,
         key: "sortOrder:",
         validator: () => true,
         getAutocompletions: () => [{ text: "descending" }, { text: "ascending" }],
-        _title: "Sort order"
+        _title: "Sort order",
+        _options: "descending, ascending"
     },
+    FILTER_SORT_BY: {
+        componentType: "FILTER",
+        regex: /sortBy:/i,
+        key: "sortBy:",
+        validator: () => true,
+        getAutocompletions: () => [{ text: "relevance" }, { text: "timestamp" }],
+        _title: "Sort by",
+        _options: "relevance, timestamp"
+    },
+    FILTER_EMBED_TYPE: {
+        componentType: "FILTER",
+        regex: /embedType:/i,
+        key: "embedType:",
+        validator: () => true,
+        getAutocompletions: () => [{ text: "gifv" }, { text: "gif" }],
+        _title: "Embed type",
+        _options: "type"
+    },
+    FILTER_FILE_NAME: {
+        componentType: "FILTER",
+        regex: /fileName:/i,
+        key: "fileName:",
+        validator: () => true,
+        getAutocompletions: () => [],
+        _title: "File name",
+        _options: "file name"
+    },
+    FILTER_FILE_TYPE: {
+        componentType: "FILTER",
+        regex: /fileType:/i,
+        key: "fileType:",
+        validator: () => true,
+        getAutocompletions: () => [{ text: "png" }, { text: "jpg" }, { text: "webp" }, { text: "gif" }, { text: "mp4" }, { text: "txt" }, { text: "js" }, { text: "css" }, { text: "zip" }],
+        _title: "File type",
+        _options: "extension"
+    },
+};
+
+const searchAnswers: {[name: string]: SearchAnswer} = {
     ANSWER_SORT_ORDER: {
         componentType: "ANSWER",
         regex: /\s*(asc|desc)(?:ending)?/i,
@@ -70,15 +107,7 @@ const searchOperators: CustomFilters = {
         queryKey: "sort_order",
         mutable: true,
         _dataKey: "sortOrder"
-    },
-    FILTER_SORT_BY: {
-        componentType: "FILTER",
-        regex: /sortBy:/i,
-        key: "sortBy:",
-        validator: () => true,
-        getAutocompletions: () => [{ text: "relevance" }, { text: "timestamp" }],
-        _title: "Sort by"
-    },
+    }, 
     ANSWER_SORT_BY: {
         componentType: "ANSWER",
         regex: /\s*(relevance|timestamp)/i,
@@ -100,13 +129,29 @@ const searchOperators: CustomFilters = {
         mutable: true,
         _dataKey: "sortBy"
     },
-    FILTER_EMBED_TYPE: {
-        componentType: "FILTER",
-        regex: /embedType:/i,
-        key: "embedType:",
-        validator: () => true,
-        getAutocompletions: () => [{ text: "gifv" }, { text: "gif" }],
-        _title: "Embed type"
+    ANSWER_FILE_NAME: {
+        componentType: "ANSWER",
+        regex: /(?:\s*([^\s]+))/,
+        validator: function (match) {
+            match.setData("fileName", match.getMatch(1));
+            return true;
+        },
+        follows: ["FILTER_FILE_NAME"],
+        queryKey: "attachment_filename",
+        mutable: true,
+        _dataKey: "fileName"
+    },
+    ANSWER_FILE_TYPE: {
+        componentType: "ANSWER",
+        regex: /(?:\s*([^\s]+))/,
+        validator: function (match) {
+            match.setData("fileType", match.getMatch(1));
+            return true;
+        },
+        follows: ["FILTER_FILE_TYPE"],
+        queryKey: "attachment_extension",
+        mutable: true,
+        _dataKey: "fileType"
     },
     ANSWER_EMBED_TYPE: {
         componentType: "ANSWER",
@@ -120,47 +165,7 @@ const searchOperators: CustomFilters = {
         mutable: true,
         _dataKey: "embedType"
     },
-    FILTER_FILE_NAME: {
-        componentType: "FILTER",
-        regex: /fileName:/i,
-        key: "fileName",
-        validator: () => true,
-        getAutocompletions: () => [],
-        _title: "File name"
-    },
-    ANSWER_FILE_NAME: {
-        componentType: "ANSWER",
-        regex: /(?:\s*([^\s]+))/,
-        validator: function (match) {
-            match.setData("fileName", match.getMatch(1));
-            return true;
-        },
-        follows: ["FILTER_FILE_NAME"],
-        queryKey: "attachment_filename",
-        mutable: true,
-        _dataKey: "fileName"
-    },
-    FILTER_FILE_TYPE: {
-        componentType: "FILTER",
-        regex: /fileType:/i,
-        key: "fileType",
-        validator: () => true,
-        getAutocompletions: () => [{ text: "png" }, { text: "jpg" }, { text: "webp" }, { text: "gif" }, { text: "mp4" }, { text: "txt" }, { text: "js" }, { text: "css" }, { text: "zip" }],
-        _title: "File type"
-    },
-    ANSWER_FILE_TYPE: {
-        componentType: "ANSWER",
-        regex: /(?:\s*([^\s]+))/,
-        validator: function (match) {
-            match.setData("fileType", match.getMatch(1));
-            return true;
-        },
-        follows: ["FILTER_FILE_TYPE"],
-        queryKey: "attachment_extension",
-        mutable: true,
-        _dataKey: "fileType"
-    }
-};
+}
 
 export default definePlugin({
     name: "MoreSearchOperators",
@@ -174,22 +179,37 @@ export default definePlugin({
         find: "Messages.SEARCH_SHORTCUT",
         replacement: {
             match: /(.)\((.),.\..{1,3}\.ANSWER_PINNED/,
-            replace: Object.keys(searchOperators).map(a => `$1($2,"${a}",$self.searchOperators.${a}),`).join("") + "$&"
+            replace: Object.keys(searchOperators).map(a => `$1($2,"${a}",$self.searchOperators.${a}),`).join("") + Object.keys(searchAnswers).map(a => `$1($2,"${a}",$self.searchAnswers.${a}),`).join("") + "$&"
         }
     }, {
         find: "Messages.SEARCH_ANSWER_FROM",
         replacement: {
             match: /var .=.\[.\];switch\(.\)\{/,
-            replace: "$&" + Object.entries(searchOperators).filter(([_, v]) => v.componentType === "ANSWER").map(([k, v]) => `case "${k}":a.add(e.getData("${v['_dataKey']}"));break;`).join("")
+            replace: "$&" + Object.entries(searchAnswers).map(([k, v]) => `case "${k}":a.add(e.getData("${v['_dataKey']}"));break;`).join("")
+        }
+    },
+    {
+        find: "Messages.SEARCH_ANSWER_FROM",
+        replacement: {
+            match: /function .\(.\)\{switch\(.\)\{/,
+            replace: "$&" + Object.entries(searchOperators).map(([k, v]) => `case \"${k}\":return \"${v['_options']}\";`).join("")
         }
     },
     {
         find: "Messages.SEARCH_GROUP_HEADER",
         replacement: {
             match: /(.{2})\((.{2}),.\..{3}\.FILTER_HAS,\{titleText:function\(\)\{return .\..\.Messages.SEARCH_GROUP_HEADER_HAS\}\}\)/,
-            replace: Object.entries(searchOperators).filter(([_, v]) => v.componentType === "FILTER").map(([k, v]) => `$1($2,\"${k}\",{titleText:()=>\"${v['_title']}\"}),`).join("") + "$&"
+            replace: Object.entries(searchOperators).map(([k, v]) => `$1($2,\"${k}\",{titleText:()=>\"${v['_title']}\"}),`).join("") + "$&"
+        }
+    },
+    {
+        find: "displayName=\"SearchAutocompleteStore\"",
+        replacement: {
+            match: /(.{2})\((.),.\..{3}\.FILTER_PINNED,!0\),/,
+            replace: "$&" + Object.keys(searchOperators).map(k => `$1($2,"${k}",true),`).join("")
         }
     }],
 
-    searchOperators
+    searchOperators,
+    searchAnswers
 });
