5ac350af6813f75961227721696c25c054786e8b
Davri <42148912+Davr1@users.noreply.github.com>
2023-07-02 20:26:37 +0200

    update plugin to match my pr

--- MoreSearchOperators/index.ts
+++ MoreSearchOperators/index.ts
@@ -18,13 +18,14 @@
 
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
+import { SelectedChannelStore } from "@webpack/common";
 
 interface SearchFilter {
     componentType: "FILTER";
     regex: RegExp;
     key: string;
-    validator: (match: any) => boolean;
-    getAutocompletions: (match: any) => { text: string; }[];
+    validator?: (match?: any) => boolean;
+    getAutocompletions: (match?: string, serverId?: string, maxResults?: number) => { text: string; }[];
     _title: string;
     _options: string;
 }
@@ -39,121 +40,28 @@ interface SearchAnswer {
     _dataKey: string | number;
 }
 
-const searchOperators: { [name: string]: SearchFilter; } = {
-    FILTER_SORT_ORDER: {
-        componentType: "FILTER",
-        regex: /sortOrder:/i,
-        key: "sortOrder:",
-        validator: () => true,
-        getAutocompletions: () => [{ text: "descending" }, { text: "ascending" }],
-        _title: "Sort order",
-        _options: "descending, ascending"
-    },
-    FILTER_SORT_BY: {
-        componentType: "FILTER",
-        regex: /sortBy:/i,
-        key: "sortBy:",
-        validator: () => true,
-        getAutocompletions: () => [{ text: "relevance" }, { text: "timestamp" }],
-        _title: "Sort by",
-        _options: "relevance, timestamp"
-    },
+const makeOptions = (...options: string[]) => options.map(text => ({ text }));
+
+const SearchOperators: Record<string, SearchFilter> = {
     FILTER_EMBED_TYPE: {
         componentType: "FILTER",
         regex: /embedType:/i,
         key: "embedType:",
-        validator: () => true,
-        getAutocompletions: () => [{ text: "image" }, { text: "video" }, { text: "gifv" }, { text: "article" }],
+        getAutocompletions: () => makeOptions("image", "video", "gifv", "article"),
         _title: "Embed type",
         _options: "type"
-    },
-    FILTER_FILE_NAME: {
-        componentType: "FILTER",
-        regex: /fileName:/i,
-        key: "fileName:",
-        validator: () => true,
-        getAutocompletions: () => [],
-        _title: "File name",
-        _options: "file name"
     },
     FILTER_FILE_TYPE: {
         componentType: "FILTER",
         regex: /fileType:/i,
         key: "fileType:",
-        validator: () => true,
-        getAutocompletions: () => [{ text: "png" }, { text: "jpg" }, { text: "webp" }, { text: "gif" }, { text: "mp4" }, { text: "txt" }, { text: "js" }, { text: "css" }, { text: "zip" }],
+        getAutocompletions: () => makeOptions("png", "jpg", "webp", "gif", "mp4", "txt", "js", "css", "zip"),
         _title: "File type",
         _options: "extension"
     },
 };
 
-const searchAnswers: { [name: string]: SearchAnswer; } = {
-    ANSWER_SORT_ORDER: {
-        componentType: "ANSWER",
-        regex: /\s*(asc|desc)(?:ending)?/i,
-        validator: function (match) {
-            let value = match.getMatch(1);
-            switch (value) {
-                case "asc":
-                    match.setData("sortOrder", "asc");
-                    return true;
-                case "desc":
-                    match.setData("sortOrder", "desc");
-                    return true;
-                default:
-                    return false;
-            }
-        },
-        follows: ["FILTER_SORT_ORDER"],
-        queryKey: "sort_order",
-        mutable: true,
-        _dataKey: "sortOrder"
-    },
-    ANSWER_SORT_BY: {
-        componentType: "ANSWER",
-        regex: /\s*(relevance|timestamp)/i,
-        validator: function (match) {
-            let value = match.getMatch(1);
-            switch (value) {
-                case "relevance":
-                    match.setData("sortBy", "relevance");
-                    return true;
-                case "timestamp":
-                    match.setData("sortBy", "timestamp");
-                    return true;
-                default:
-                    return false;
-            }
-        },
-        follows: ["FILTER_SORT_BY"],
-        queryKey: "sort_by",
-        mutable: true,
-        _dataKey: "sortBy"
-    },
-    ANSWER_FILE_NAME: {
-        componentType: "ANSWER",
-        regex: /(?:\s*([^\s]+))/,
-        validator: function (match) {
-            match.setData("fileName", match.getMatch(1));
-            return true;
-        },
-        follows: ["FILTER_FILE_NAME"],
-        queryKey: "attachment_filename",
-        mutable: true,
-        _dataKey: "fileName"
-    },
-    ANSWER_FILE_TYPE: {
-        componentType: "ANSWER",
-        regex: /(?:\s*([^\s]+))/,
-        validator: function (match) {
-            match.setData("fileType", match.getMatch(1));
-            return true;
-        },
-        follows: ["FILTER_FILE_TYPE"],
-        queryKey: "attachment_extension",
-        mutable: true,
-        _dataKey: "fileType"
-    },
+const SearchAnswers: Record<string, SearchAnswer> = {
     ANSWER_EMBED_TYPE: {
         componentType: "ANSWER",
         regex: /\s*([^\s]+)/i,
@@ -166,48 +74,102 @@ const searchAnswers: { [name: string]: SearchAnswer; } = {
         mutable: true,
         _dataKey: "embedType"
     },
+    ANSWER_FILE_TYPE: {
+        componentType: "ANSWER",
+        regex: /(?:\s*([^\s]+))/,
+        validator(match) {
+            match.setData("fileType", match.getMatch(1));
+            return true;
+        },
+        follows: ["FILTER_FILE_TYPE"],
+        queryKey: "attachment_extension",
+        mutable: true,
+        _dataKey: "fileType"
+    },
 };
+
+function registerSearchOperators(originalOperators: Record<string, SearchFilter | SearchAnswer>) {
+    Object.assign(originalOperators, SearchOperators, SearchAnswers);
+}
+
+// Makes all of the custom operators visible by default
+function setAsVisible(originalOperators: Record<string, boolean>) {
+    for (const name in SearchOperators) {
+        originalOperators[name] = true; // null for hidden
+    }
+}
+
+// Defines the text displayed above the list of autocompletions
+function setHeaderText(originalOperators: Record<string, { titleText(): string; }>) {
+    for (const [name, operator] of Object.entries(SearchOperators)) {
+        originalOperators[name] = { titleText: () => operator._title };
+    }
+}
+
+function getSearchType(searchQuery: { query: any, searchId: string; }): [id: string, type: "CHANNEL" | "GUILD"] {
+    const { query, searchId } = searchQuery;
+    if (searchId === "@favorites" || "embed_type" in query) {
+        return [query.channel_id?.[0] ?? SelectedChannelStore.getChannelId(), "CHANNEL"];
+    }
+    return [searchId, "GUILD"];
+}
 
 export default definePlugin({
     name: "MoreSearchOperators",
     description: "Adds experimental search operators.",
     authors: [Devs.Davri],
 
-    patches: [{
-        find: "Messages.SEARCH_SHORTCUT",
-        replacement: {
-            match: /(\i)\((\i),\i\.\i\.ANSWER_PINNED/,
-            replace: Object.keys(searchOperators).map(a => `$1($2,"${a}",$self.searchOperators.${a}),`).join("") + Object.keys(searchAnswers).map(a => `$1($2,"${a}",$self.searchAnswers.${a}),`).join("") + "$&"
+    patches: [
+        {
+            find: "Messages.SEARCH_SHORTCUT",
+            replacement: {
+                match: /\i\((\i),\i\.\i\.ANSWER_PINNED/,
+                replace: "$self.registerSearchOperators($1),$&"
+            }
+        },
+        {
+            find: "Messages.SEARCH_ANSWER_FROM",
+            replacement: {
+                match: /(?<=\i\.forEach\(\(function\((\i)\).{300,500})var (\i)=\i\[\i\];switch\(\i\)\{/,
+                // Adds query parameters to the search url
+                replace: "$&" + Object.entries(SearchAnswers).map(([k, v]) => `case "${k}":$2.add($1.getData("${v._dataKey}"));break;`).join("")
+            }
+        },
+        {
+            find: "Messages.SEARCH_ANSWER_FROM",
+            replacement: {
+                match: /function \i\(\i\)\{switch\(\i\)\{/,
+                // Defines the example text next to the initial list of autocompletions
+                replace: "$&" + Object.entries(SearchOperators).map(([k, v]) => `case "${k}":return "${v._options}";`).join("")
+            }
+        },
+        {
+            find: "Messages.SEARCH_GROUP_HEADER",
+            replacement: {
+                match: /\i\((\i),\i\.\i\.FILTER_HAS,\{titleText/,
+                replace: "$self.setHeaderText($1),$&"
+            }
+        },
+        {
+            find: "displayName=\"SearchAutocompleteStore\"",
+            replacement: {
+                match: /\i\((\i),\i\.\i\.FILTER_PINNED,!0\),/,
+                replace: "$self.setAsVisible($1),$&"
+            }
+        },
+        {
+            find: "displayName=\"SearchStore\"",
+            replacement: {
+                match: /(?<=SEARCH_START:function\((\i)\).{100,200})var (\i)=\i,(\i)=\i\.searchType;if\(\i===\i\.\i\)\{.{50,100}\}/,
+                replace: "var [$2,$3]=$self.getSearchType($1);"
+            }
         }
-    }, {
-        find: "Messages.SEARCH_ANSWER_FROM",
-        replacement: {
-            match: /(?<=\i\.forEach\(\(function\((\i)\).{300,500})var (\i)=\i\[\i\];switch\(\i\)\{/,
-            replace: "$&" + Object.entries(searchAnswers).map(([k, v]) => `case "${k}":$2.add($1.getData("${v['_dataKey']}"));break;`).join("")
-        }
-    },
-    {
-        find: "Messages.SEARCH_ANSWER_FROM",
-        replacement: {
-            match: /function \i\(\i\)\{switch\(\i\)\{/,
-            replace: "$&" + Object.entries(searchOperators).map(([k, v]) => `case \"${k}\":return \"${v['_options']}\";`).join("")
-        }
-    },
-    {
-        find: "Messages.SEARCH_GROUP_HEADER",
-        replacement: {
-            match: /(\i)\((\i),\i\.\i\.FILTER_HAS,\{titleText:function\(\)\{return \i\.\i\.Messages.SEARCH_GROUP_HEADER_HAS\}\}\)/,
-            replace: Object.entries(searchOperators).map(([k, v]) => `$1($2,\"${k}\",{titleText:()=>\"${v['_title']}\"}),`).join("") + "$&"
-        }
-    },
-    {
-        find: "displayName=\"SearchAutocompleteStore\"",
-        replacement: {
-            match: /(\i)\((\i),\i\.\i\.FILTER_PINNED,!0\),/,
-            replace: "$&" + Object.keys(searchOperators).map(k => `$1($2,"${k}",true),`).join("")
-        }
-    }],
+    ],
 
-    searchOperators,
-    searchAnswers
+    searchOperators: SearchOperators,
+    searchAnswers: SearchAnswers,
+    registerSearchOperators,
+    setAsVisible,
+    setHeaderText,
+    getSearchType
 });
